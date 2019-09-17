62ef372a5165b9de587207241c2eee4df1292a80
Maksim <maksim.lv@mail.ru>
2019-09-17 15:29:07 +0300

    Fix for AJAX script. You have to stringify a data before sending it to server

--- static/ajax.js
+++ static/ajax.js
@@ -2,16 +2,16 @@ window.onload = function () {
     document.getElementById('submit').addEventListener('click', (e) => {
         e.preventDefault();
 
-        let formName = document.forms['formName'];
-        let longLink = formName.elements['link'].value;
-        let request = new XMLHttpRequest();
-        let linkRes = document.getElementById('linkRes');
+        const value = document.forms['formName'].value;
+/*        let longLink = formName.elements['link'].value;
+        let linkRes = document.getElementById('linkRes'); */
 
+        let request = new XMLHttpRequest();
         request.open('POST', '/api/link', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.addEventListener('load', () => {
-            linkRes.value = 'http://localhost:3000/url/' + request.response;
+            linkRes.value = 'http://localhost:3000/url/' + JSON.parse(request.response);
         });
-        request.send(longLink);
+        request.send(JSON.stringify(value));
     });
 };
