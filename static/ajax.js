406b656f76ccb24baac5e7be62c05852bfae26b6
Aleksey Gurin <gurin.aleksey@gmail.com>
2019-09-26 15:21:08 +0300

    check request for empty link

--- static/ajax.js
+++ static/ajax.js
@@ -6,15 +6,19 @@ window.onload = function () {
         let longLink = formName.elements['link'].value;
         let with_button_checkbox = formName.elements['with_button_checkbox'].checked;
 
-        let request = new XMLHttpRequest();
-        request.open('POST', '/api/link', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.addEventListener('load', () => {
-            let linkRes = document.getElementById('linkRes');
-            //Example: JSON.parse('"foo"');  // "foo"
-            linkRes.value = 'http://localhost:3000/res/' + JSON.parse(request.response);
-        });
-        //Example: JSON.stringify({ x: 5, y: 6 }); //'{"x":5,"y":6}' or '{"y":6,"x":5}'
-        request.send(JSON.stringify({ link: longLink, checkbox: with_button_checkbox}));
+        if(longLink !== '') {
+            let request = new XMLHttpRequest();
+            request.open('POST', '/api/link', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.addEventListener('load', () => {
+                let linkRes = document.getElementById('linkRes');
+                //Example: JSON.parse('"foo"');  // "foo"
+                formName.elements['link'].value = '';
+                linkRes.value = 'http://localhost:3000/res/' + JSON.parse(request.response);
+            });
+
+                //Example: JSON.stringify({ x: 5, y: 6 }); //'{"x":5,"y":6}' or '{"y":6,"x":5}'
+                request.send(JSON.stringify({link: longLink, checkbox: with_button_checkbox}));
+        }
     });
 };
